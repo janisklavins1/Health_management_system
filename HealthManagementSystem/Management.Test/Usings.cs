e708df9acbff7264ab154dde1514f33c98d92fe9
Jānis Kļaviņš <jklavins14@gmail.com>
2023-03-15 20:46:09 +0200

    Added unit test for AllergyToPersonController

---
+++ HealthManagementSystem/Management.Test/Usings.cs
@@ -0,0 +1,8 @@
+global using Xunit;
+global using Moq;
+global using FluentAssertions;
+global using HealthManagementSystem.Dto;
+global using Management.Application.Interfaces;
+global using HealthManagementSystem.Controllers;
+global using Management.Application.Dto;
+global using Management.Domain.Models;
